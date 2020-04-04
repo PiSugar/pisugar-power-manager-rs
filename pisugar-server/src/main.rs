eafc0d1aa7d0913381b5b55e1e27ee7a3e092329
fengyingcai <fengyc.work@gmail.com>
2020-04-04 12:08:46 +0800

    Update

--- pisugar-server/src/main.rs
+++ pisugar-server/src/main.rs
@@ -377,10 +377,10 @@ async fn serve_http(http_addr: SocketAddr, web_dir: String) {
 async fn main() -> std::io::Result<()> {
     env_logger::init();
 
-    let matches = App::new("PiSugar Power Manager")
-        .version("1.0")
-        .author("PiSugar")
-        .about("PiSugar power management module.")
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
         .arg(
             Arg::with_name("config")
                 .short("c")
