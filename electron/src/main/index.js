4c453a1c83e29b3904bafa2d5b5617466a2beac3
JdaieLin <490844515@qq.com>
2021-08-08 10:16:10 +0800

    fix: electron process

--- electron/src/main/index.js
+++ electron/src/main/index.js
@@ -77,7 +77,8 @@ function createWindow () {
     resizable: true,
     webPreferences: {
       webSecurity: false,
-      nodeIntegration: true
+      nodeIntegration: true,
+      nodeIntegrationInWorker: true,
     }
   })
 
