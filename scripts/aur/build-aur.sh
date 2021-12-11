fd41649ba4c24f39317a40ec88d7e52937a73147
fengyc <fengyc.work@gmail.com>
2021-12-12 00:05:25 +0800

    fix: ArchLinux script

--- scripts/aur/build-aur.sh
+++ scripts/aur/build-aur.sh
@@ -24,16 +24,14 @@ version=$(cat PKGBUILD | grep ^pkgver | awk -F = '{print $2}')
 mkdir arm
 mkdir armhf
 mkdir aarch64
-mkdir x86_64
 
 for i in server poweroff programmer; do
     cp "$ROOT_DIR/target/arm-unknown-linux-musleabi/release/pisugar-$i" arm/
     cp "$ROOT_DIR/target/arm-unknown-linux-musleabihf/release/pisugar-$i" armhf/
     cp "$ROOT_DIR/target/aarch64-unknown-linux-musl/release/pisugar-$i" aarch64/
-    cp "$ROOT_DIR/target/x86_64-unknown-linux-gnu/release/pisugar-$i" x86_64/
 done
 
-for i in arm armhf aarch64 x86_64; do
+for i in arm armhf aarch64; do
     cp -r "$ROOT_DIR/pisugar-server/.rpm/_ws.json" $i/
     cp -r "$ROOT_DIR/pisugar-server/.rpm/config.json" $i/
     cp -r "$ROOT_DIR/pisugar-server/.rpm/pisugar-server.default" $i/
@@ -43,8 +41,8 @@ for i in arm armhf aarch64 x86_64; do
     cp -r "$ROOT_DIR/pisugar-poweroff/.rpm/pisugar-poweroff.service" $i/
 done
 
-tar -czvf pisugar-bin_${version}_all.tar.gz arm/ armhf/ aarch64/ x86_64/
-rm -rf arm armhf aarch64 x86_64
+tar -czvf pisugar-bin_${version}_all.tar.gz arm/ armhf/ aarch64/
+rm -rf arm armhf aarch64
 
 echo "sha256sums=('$(sha256sum pisugar-bin_${version}_all.tar.gz | awk '{print $1}')')" >> PKGBUILD
 
