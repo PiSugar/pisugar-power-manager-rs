feb64e37506313933e183a8b97223d59ba1da3fc
铲屎将军 <37292630+wlmh110@users.noreply.github.com>
2022-03-03 12:12:27 +0800

    Add files via upload

---
+++ scripts/BootWatchdogPiSugar3.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+TMP=$(i2cget -y 1 0x57 0x06)
+#echo $TMP
+RST=$((0x18 | TMP ))#开机看门狗开启并喂狗 Turn on the watchdog and feed the dog
+#echo $RST
+
+i2cset -y 1 0x57 0x0a 10 #设置最大重启次数 Set the maximum number of restarts
+i2cset -y 1 0x57 0x06 $RST #写入寄存器 Write register
+
+#该脚本应当设置为开机启动。每次开机只需要运行一次 The script should be set to boot. You only need to run it once per boot
