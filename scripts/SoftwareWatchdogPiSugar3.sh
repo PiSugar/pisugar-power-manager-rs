1ef741cafa18559a5a25bfbbcd04cb30b4859652
铲屎将军 <37292630+wlmh110@users.noreply.github.com>
2022-03-03 12:05:44 +0800

    Update SoftwareWatchdogPiSugar3.sh

--- scripts/SoftwareWatchdogPiSugar3.sh
+++ scripts/SoftwareWatchdogPiSugar3.sh
@@ -8,6 +8,8 @@ i2cset -y 1 0x57 0x07 10 #设置超时时长10*2s Set timeout duration 10 * 2S
 i2cset -y 1 0x57 0x06 $RST #写入寄存器 Write register
 #i2cdump -y 1 0x57
 
+
+#需要不断的周期性喂狗，否则系统会重新启动 You need to feed the dog regularly, otherwise the system will restart
 while [ 1 ]; do
 TMP=$(i2cget -y 1 0x57 0x06)
 #echo $TMP  >> /home/pi/wdlog.txt
