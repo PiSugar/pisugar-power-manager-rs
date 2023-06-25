47769bc4698232e6dbeebd2ee37bc0cdee728d20
fengyc <fengyc.work@gmail.com>
2023-06-25 22:42:50 +0800

    feat: kernel module

--- pisugar-module/README.md
+++ pisugar-module/README.md
@@ -1,14 +1,63 @@
 # PiSugar Linux kernel modules
 
-Make kernel modules:
+Linux kernel modules for PiSugar 3.
+
+## Preparing for building RPI kernel module
+
+### Linux distributions with kernel symbols
+
+Congratulations if your PI is running a linux distribution that has `/lib/modules/$(uname -r)/build/` directory, e.g. ubuntu-server, you don't need to manually build RPI kernel, and that will save a lot of time.
+
+Install linux-headers
+```shell
+sudo apt install linux-headers-$(uname -r)
+```
+
+### Raspberry Pi OS
+
+As kernel symbols is not included in Raspberry Pi OS (no `/lib/modules/$(uname -r)/build`), so you need to compile the kernel and generate the kernel symbols by youself. 
+
+To build the kernel, see official doc: https://www.raspberrypi.com/documentation/computers/linux_kernel.html
+
+** It seems like the precompiled symbols could be downloaded from [here](https://github.com/raspberrypi/firmware), but I could not figure out the correct steps. **
+
+First, get RPI OS tag, e.g. 1.20230405
+```shell
+dpkg -l | grep kernel
+```
+
+Clone kernel repository, and create a symbol link
+```shell
+git clone --depth 1 --branch <PI_OS_TAG> https://github.com/raspberrypi/linux.git
+sudo ln -s "$(pwd)/linux" /usr/src/linux
+```
+
+Build RPI kernel.
+
+When it is done, copy and rename `linux` folder to PI `/lib/modules/$(uname -r)/build`.
+
+## Compiling kernel module
+
+Clone this repository, make kernel modules:
 
     make
 
 Load module:
 
     sudo insmod pisugar_3_battery.ko
+    # or
+    sudo insmod pisugar_3_battery.ko i2c_bus=1 i2c_addr=0x57
+
+Then you will see extra battry files in `/sys/class/power_supply`
+
+    ls /sys/class/power_supply
 
 Remove module:
 
     sudo rmmod pisugar_3_battery.ko
 
+Now, you can enable [battery monitor plugin](https://github.com/raspberrypi-ui/lxplug-ptbatt).
+
+## License
+
+GPL
--- pisugar-module/pisugar_3_battery.c
+++ pisugar-module/pisugar_3_battery.c
@@ -13,56 +13,80 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/* Based heavily on https://git.kernel.org/cgit/linux/kernel/git/stable/linux-stable.git/tree/drivers/power/test_power.c?id=refs/tags/v4.2.6 */
+/* Based heavily on
+ * https://git.kernel.org/cgit/linux/kernel/git/stable/linux-stable.git/tree/drivers/power/test_power.c?id=refs/tags/v4.2.6
+ */
 /* Fork from https://github.com/hoelzro/linux-fake-battery-module */
 
-#include <asm/uaccess.h>
 #include <linux/fs.h>
+#include <linux/i2c.h>
+#include <linux/init.h>
 #include <linux/kernel.h>
+#include <linux/kthread.h>
 #include <linux/miscdevice.h>
 #include <linux/module.h>
 #include <linux/power_supply.h>
+#include <linux/sched.h>
 
-static int fake_battery_get_property1(struct power_supply *psy,
-                                      enum power_supply_property psp,
-                                      union power_supply_propval *val);
+#define PISUGAR_3_BAT_I2C_BUS  0x01
+#define PISUGAR_3_BAT_I2C_ADDR 0x57
 
-static int fake_battery_get_property2(struct power_supply *psy,
-                                      enum power_supply_property psp,
-                                      union power_supply_propval *val);
+enum pisugar_3_bat_reg {
+    PISUGAR_3_VER = 0x00,
+    PISUGAR_3_MOD = 0x01,
+    PISUGAR_3_CTL1 = 0x02,
+    PISUGAR_3_TEMP = 0x04,
+    PISUGAR_3_CAP = 0x2A,
+    PISUGAR_3_VOL_H = 0x22,
+    PISGUAR_3_VOL_L = 0x23,
+};
 
-static int fake_ac_get_property(struct power_supply *psy,
-                                enum power_supply_property psp,
-                                union power_supply_propval *val);
+#define PISUGAR_3_VER_3   3
+#define PISUGAR_3_MOD_APP 0x0F
+
+#define PISUGAR_3_MSK_CTR1_USB   (1 << 7)
+#define PISUGAR_3_MSK_CTR1_CH_EN (1 << 6)
+
+static short int i2c_bus = PISUGAR_3_BAT_I2C_BUS;
+static short int i2c_addr = PISUGAR_3_BAT_I2C_ADDR;
+
+module_param(i2c_bus, short, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+MODULE_PARM_DESC(i2c_bus, "I2C bus default 0x01");
+
+module_param(i2c_addr, short, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+MODULE_PARM_DESC(i2c_addr, "I2C addr default 0x57");
+
+static int pisugar_3_battery_get_property1(struct power_supply *psy,
+                                           enum power_supply_property psp,
+                                           union power_supply_propval *val);
+
+static int pisugar_3_ac_get_property(struct power_supply *psy,
+                                     enum power_supply_property psp,
+                                     union power_supply_propval *val);
+
+static struct task_struct *pisugar_3_monitor_task = NULL;
 
 static struct battery_status {
     int status;
     int capacity_level;
     int capacity;
     int time_left;
-} fake_battery_statuses[2] = {
-    {
-        .status = POWER_SUPPLY_STATUS_FULL,
-        .capacity_level = POWER_SUPPLY_CAPACITY_LEVEL_FULL,
-        .capacity = 100,
-        .time_left = 3600,
-    },
-    {
-        .status = POWER_SUPPLY_STATUS_FULL,
-        .capacity_level = POWER_SUPPLY_CAPACITY_LEVEL_FULL,
-        .capacity = 100,
-        .time_left = 3600,
-    },
-};
+    int voltage;
+    int temperature;
+} pisugar_3_battery_statuses[1] = {{
+    .status = POWER_SUPPLY_STATUS_FULL,
+    .capacity_level = POWER_SUPPLY_CAPACITY_LEVEL_FULL,
+    .capacity = 100,
+    .time_left = 3600,
+}};
 
 static int ac_status = 1;
 
-static char *fake_ac_supplies[] = {
+static char *pisugar_3_ac_supplies[] = {
     "BAT0",
-    "BAT1",
 };
 
-static enum power_supply_property fake_battery_properties[] = {
+static enum power_supply_property pisugar_3_battery_properties[] = {
     POWER_SUPPLY_PROP_STATUS,
     POWER_SUPPLY_PROP_CHARGE_TYPE,
     POWER_SUPPLY_PROP_HEALTH,
@@ -82,7 +106,7 @@ static enum power_supply_property fake_battery_properties[] = {
     POWER_SUPPLY_PROP_VOLTAGE_NOW,
 };
 
-static enum power_supply_property fake_ac_properties[] = {
+static enum power_supply_property pisugar_3_ac_properties[] = {
     POWER_SUPPLY_PROP_ONLINE,
 };
 
@@ -90,25 +114,17 @@ static struct power_supply_desc descriptions[] = {
     {
         .name = "BAT0",
         .type = POWER_SUPPLY_TYPE_BATTERY,
-        .properties = fake_battery_properties,
-        .num_properties = ARRAY_SIZE(fake_battery_properties),
-        .get_property = fake_battery_get_property1,
-    },
-
-    {
-        .name = "BAT1",
-        .type = POWER_SUPPLY_TYPE_BATTERY,
-        .properties = fake_battery_properties,
-        .num_properties = ARRAY_SIZE(fake_battery_properties),
-        .get_property = fake_battery_get_property2,
+        .properties = pisugar_3_battery_properties,
+        .num_properties = ARRAY_SIZE(pisugar_3_battery_properties),
+        .get_property = pisugar_3_battery_get_property1,
     },
 
     {
         .name = "AC0",
         .type = POWER_SUPPLY_TYPE_MAINS,
-        .properties = fake_ac_properties,
-        .num_properties = ARRAY_SIZE(fake_ac_properties),
-        .get_property = fake_ac_get_property,
+        .properties = pisugar_3_ac_properties,
+        .num_properties = ARRAY_SIZE(pisugar_3_ac_properties),
+        .get_property = pisugar_3_ac_get_property,
     },
 };
 
@@ -116,176 +132,29 @@ static struct power_supply_config configs[] = {
     {},
     {},
     {
-        .supplied_to = fake_ac_supplies,
-        .num_supplicants = ARRAY_SIZE(fake_ac_supplies),
+        .supplied_to = pisugar_3_ac_supplies,
+        .num_supplicants = ARRAY_SIZE(pisugar_3_ac_supplies),
     },
 };
 
 static struct power_supply *supplies[sizeof(descriptions) / sizeof(descriptions[0])];
 
-static ssize_t control_device_read(struct file *file, char *buffer, size_t count, loff_t *ppos)
-{
-    static char *message = "fake battery information!";
-    size_t message_len = strlen(message);
-
-    if (count < message_len) {
-        return -EINVAL;
-    }
-
-    if (*ppos != 0) {
-        return 0;
-    }
-
-    if (copy_to_user(buffer, message, message_len)) {
-        return -EINVAL;
-    }
-
-    *ppos = message_len;
-
-    return message_len;
-}
-
 #define prefixed(s, prefix) (!strncmp((s), (prefix), sizeof(prefix) - 1))
 
-static int handle_control_line(const char *line, int *ac_status, struct battery_status *batteries)
-{
-    char *value_p;
-    long value;
-    int status;
-
-    value_p = strchrnul(line, '=');
-
-    if (!value_p) {
-        return -EINVAL;
-    }
-
-    value_p = skip_spaces(value_p + 1);
-
-    status = kstrtol(value_p, 10, &value);
-
-    if (status) {
-        return status;
-    }
-
-    if (prefixed(line, "capacity")) {
-        int battery_num = line[sizeof("capacity") - 1] - '0';
-        if (battery_num != 0 && battery_num != 1) {
-            return -ERANGE;
-        }
-        batteries[battery_num].capacity = value;
-    } else if (prefixed(line, "charging")) {
-        *ac_status = value;
-    } else {
-        return -EINVAL;
-    }
-
-    return 0;
-}
-
-static void handle_charge_changes(int ac_status, struct battery_status *battery)
-{
-    if (ac_status) {
-        if (battery->capacity < 100) {
-            battery->status = POWER_SUPPLY_STATUS_CHARGING;
-        } else {
-            battery->status = POWER_SUPPLY_STATUS_FULL;
-        }
-    } else {
-        battery->status = POWER_SUPPLY_STATUS_DISCHARGING;
-    }
-
-    if (battery->capacity >= 98) {
-        battery->capacity_level = POWER_SUPPLY_CAPACITY_LEVEL_FULL;
-    } else if (battery->capacity_level >= 70) {
-        battery->capacity_level = POWER_SUPPLY_CAPACITY_LEVEL_HIGH;
-    } else if (battery->capacity_level >= 30) {
-        battery->capacity_level = POWER_SUPPLY_CAPACITY_LEVEL_NORMAL;
-    } else if (battery->capacity_level >= 5) {
-        battery->capacity_level = POWER_SUPPLY_CAPACITY_LEVEL_LOW;
-    } else {
-        battery->capacity_level = POWER_SUPPLY_CAPACITY_LEVEL_CRITICAL;
-    }
-
-    battery->time_left = 36 * battery->capacity;
-}
-
-static ssize_t control_device_write(struct file *file, const char *buffer, size_t count, loff_t *ppos)
-{
-    char kbuffer[1024]; /* limited by kernel frame size, 1K should be enough */
-    char *buffer_cursor;
-    char *newline;
-    size_t bytes_left = count;
-
-    int status;
-
-    if (*ppos != 0) {
-        printk(KERN_ERR "writes to /dev/fake_battery must be completed in a single system call\n");
-        return -EINVAL;
-    }
-
-    if (count > 1024) {
-        printk(KERN_ERR "Too much data provided to /dev/fake_battery (limit 1024 bytes)\n");
-        return -EINVAL;
-    }
-
-    status = copy_from_user(kbuffer, buffer, count);
-
-    if (status != 0) {
-        printk(KERN_ERR "bad copy_from_user\n");
-        return -EINVAL;
-    }
-
-    buffer_cursor = kbuffer;
-
-    while ((newline = memchr(buffer_cursor, '\n', bytes_left))) {
-        *newline = '\0';
-        /* XXX this is non-atomic */
-        status = handle_control_line(buffer_cursor, &ac_status, fake_battery_statuses);
-
-        if (status) {
-            return status;
-        }
-
-        bytes_left -= (newline - buffer_cursor) + 1;
-        buffer_cursor = newline + 1;
-    }
-
-    handle_charge_changes(ac_status, &fake_battery_statuses[0]);
-    handle_charge_changes(ac_status, &fake_battery_statuses[1]);
-
-    power_supply_changed(supplies[0]);
-    power_supply_changed(supplies[1]);
-    power_supply_changed(supplies[2]);
-
-    return count;
-}
-
-static struct file_operations control_device_ops = {
-    .owner = THIS_MODULE,
-    .read = control_device_read,
-    .write = control_device_write,
-};
-
-static struct miscdevice control_device = {
-    MISC_DYNAMIC_MINOR,
-    "fake_battery",
-    &control_device_ops,
-};
-
-static int fake_battery_generic_get_property(struct power_supply *psy,
-                                             enum power_supply_property psp,
-                                             union power_supply_propval *val,
-                                             struct battery_status *status)
+static int pisugar_3_battery_generic_get_property(struct power_supply *psy,
+                                                  enum power_supply_property psp,
+                                                  union power_supply_propval *val,
+                                                  struct battery_status *status)
 {
     switch (psp) {
         case POWER_SUPPLY_PROP_MANUFACTURER:
-            val->strval = "Linux";
+            val->strval = "PiSugar";
             break;
         case POWER_SUPPLY_PROP_STATUS:
             val->intval = status->status;
             break;
         case POWER_SUPPLY_PROP_CHARGE_TYPE:
-            val->intval = POWER_SUPPLY_CHARGE_TYPE_FAST;
+            val->intval = POWER_SUPPLY_CHARGE_TYPE_STANDARD;
             break;
         case POWER_SUPPLY_PROP_HEALTH:
             val->intval = POWER_SUPPLY_HEALTH_GOOD;
@@ -296,10 +165,12 @@ static int fake_battery_generic_get_property(struct power_supply *psy,
         case POWER_SUPPLY_PROP_TECHNOLOGY:
             val->intval = POWER_SUPPLY_TECHNOLOGY_LION;
             break;
+        case POWER_SUPPLY_PROP_CAPACITY:
+            val->intval = status->capacity;
+            break;
         case POWER_SUPPLY_PROP_CAPACITY_LEVEL:
             val->intval = status->capacity_level;
             break;
-        case POWER_SUPPLY_PROP_CAPACITY:
         case POWER_SUPPLY_PROP_CHARGE_NOW:
             val->intval = status->capacity;
             break;
@@ -312,10 +183,10 @@ static int fake_battery_generic_get_property(struct power_supply *psy,
             val->intval = status->time_left;
             break;
         case POWER_SUPPLY_PROP_TEMP:
-            val->intval = 26;
+            val->intval = status->temperature;
             break;
         case POWER_SUPPLY_PROP_VOLTAGE_NOW:
-            val->intval = 3300;
+            val->intval = status->voltage;
             break;
         default:
             pr_info("%s: some properties deliberately report errors.\n", __func__);
@@ -324,43 +195,26 @@ static int fake_battery_generic_get_property(struct power_supply *psy,
     return 0;
 };
 
-static int fake_battery_get_property1(struct power_supply *psy,
-                                      enum power_supply_property psp,
-                                      union power_supply_propval *val)
+static int pisugar_3_battery_get_property1(struct power_supply *psy,
+                                           enum power_supply_property psp,
+                                           union power_supply_propval *val)
 {
     switch (psp) {
         case POWER_SUPPLY_PROP_MODEL_NAME:
-            val->strval = "Fake battery 1";
+            val->strval = "PiSugar battery 0";
             break;
         case POWER_SUPPLY_PROP_SERIAL_NUMBER:
-            val->strval = "12345678";
+            val->strval = "";
             break;
         default:
-            return fake_battery_generic_get_property(psy, psp, val, &fake_battery_statuses[0]);
+            return pisugar_3_battery_generic_get_property(psy, psp, val, &pisugar_3_battery_statuses[0]);
     }
     return 0;
 }
 
-static int fake_battery_get_property2(struct power_supply *psy,
-                                      enum power_supply_property psp,
-                                      union power_supply_propval *val)
-{
-    switch (psp) {
-        case POWER_SUPPLY_PROP_MODEL_NAME:
-            val->strval = "Fake battery 2";
-            break;
-        case POWER_SUPPLY_PROP_SERIAL_NUMBER:
-            val->strval = "12345678";
-            break;
-        default:
-            return fake_battery_generic_get_property(psy, psp, val, &fake_battery_statuses[1]);
-    }
-    return 0;
-}
-
-static int fake_ac_get_property(struct power_supply *psy,
-                                enum power_supply_property psp,
-                                union power_supply_propval *val)
+static int pisugar_3_ac_get_property(struct power_supply *psy,
+                                     enum power_supply_property psp,
+                                     union power_supply_propval *val)
 {
     switch (psp) {
         case POWER_SUPPLY_PROP_ONLINE:
@@ -372,50 +226,148 @@ static int fake_ac_get_property(struct power_supply *psy,
     return 0;
 }
 
-static int __init fake_battery_init(void)
+#define CHECK_VALID(val) ((val) >= 0 && (val) <= 255)
+
+static int pisugar_3_monitor(void *args)
+{
+    struct i2c_client *pisugar_3_client = NULL;
+    struct i2c_adapter *adapter = NULL;
+    struct i2c_board_info board_info = {I2C_BOARD_INFO("pisugar_3_battery", i2c_addr)};
+
+    // create i2c pisugar_3_client
+    adapter = i2c_get_adapter(i2c_bus);
+    if (adapter == NULL) {
+        printk(KERN_ERR "Unable to get i2c adapter!");
+        return -1;
+    }
+    pisugar_3_client = i2c_new_client_device(adapter, &board_info);
+    if (pisugar_3_client == NULL) {
+        printk(KERN_ERR "Unable to create i2c client!");
+        return -1;
+    }
+
+    while (true) {
+        set_current_state(TASK_UNINTERRUPTIBLE);
+        if (kthread_should_stop()) break;
+
+        int ver = i2c_smbus_read_byte_data(pisugar_3_client, PISUGAR_3_VER);
+        int mode = i2c_smbus_read_byte_data(pisugar_3_client, PISUGAR_3_MOD);
+        if (ver != PISUGAR_3_VER_3 || mode != PISUGAR_3_MOD_APP) {
+            ac_status = 0;  // device offlie
+        } else {
+            int ctl1 = i2c_smbus_read_byte_data(pisugar_3_client, PISUGAR_3_CTL1);
+            if (!CHECK_VALID(ctl1)) {
+                goto sleep;
+            }
+            bool ch_en = ctl1 & PISUGAR_3_MSK_CTR1_CH_EN;
+            bool online = ctl1 & PISUGAR_3_MSK_CTR1_USB ? 1 : 0;
+            ac_status = online ? 1 : 0;
+
+            // temperature, zero point -40c
+            int temperature = i2c_smbus_read_byte_data(pisugar_3_client, PISUGAR_3_TEMP);
+            if (!CHECK_VALID(temperature)) {
+                goto sleep;
+            }
+            pisugar_3_battery_statuses->temperature = temperature - 40;
+
+            // battery capacity
+            int cap = i2c_smbus_read_byte_data(pisugar_3_client, PISUGAR_3_CAP);
+            cap = cap > 100 ? 100 : cap;
+            pisugar_3_battery_statuses->capacity = cap;
+            if (cap > 98) {
+                pisugar_3_battery_statuses->capacity_level = POWER_SUPPLY_CAPACITY_LEVEL_FULL;
+            } else if (cap > 90) {
+                pisugar_3_battery_statuses->capacity_level = POWER_SUPPLY_CAPACITY_LEVEL_HIGH;
+            } else if (cap > 50) {
+                pisugar_3_battery_statuses->capacity_level = POWER_SUPPLY_CAPACITY_LEVEL_NORMAL;
+            } else if (cap > 30) {
+                pisugar_3_battery_statuses->capacity_level = POWER_SUPPLY_CAPACITY_LEVEL_LOW;
+            } else {
+                pisugar_3_battery_statuses->capacity_level = POWER_SUPPLY_CAPACITY_LEVEL_CRITICAL;
+            }
+
+            // voltage
+            int vol_h = i2c_smbus_read_byte_data(pisugar_3_client, PISUGAR_3_VOL_H);
+            int vol_l = i2c_smbus_read_byte_data(pisugar_3_client, PISGUAR_3_VOL_L);
+            if (!CHECK_VALID(vol_h) || !CHECK_VALID(vol_l)) {
+                goto sleep;
+            }
+            pisugar_3_battery_statuses->voltage = (vol_h << 8) | vol_l;
+
+            // charging status
+            if (online && ch_en) {
+                if (cap > 98) {
+                    pisugar_3_battery_statuses->status = POWER_SUPPLY_STATUS_FULL;
+                } else {
+                    pisugar_3_battery_statuses->status = POWER_SUPPLY_STATUS_CHARGING;
+                }
+            } else {
+                pisugar_3_battery_statuses->status = POWER_SUPPLY_STATUS_DISCHARGING;
+            }
+        }
+    sleep:
+        schedule_timeout(HZ);
+    }
+
+    i2c_unregister_device(pisugar_3_client);
+    pisugar_3_client = NULL;
+
+    return 0;
+}
+
+static int __init pisugar_3_battery_init(void)
 {
     int result;
     int i;
 
-    result = misc_register(&control_device);
-    if (result) {
-        printk(KERN_ERR "Unable to register misc device!");
-        return result;
+    // create a monitor kthread
+    pisugar_3_monitor_task = kthread_run(pisugar_3_monitor, NULL, "pisugar_3_monitor");
+    if (pisugar_3_monitor_task == NULL) {
+        goto error;
     }
 
+    // register power supply
     for (i = 0; i < ARRAY_SIZE(descriptions); i++) {
         supplies[i] = power_supply_register(NULL, &descriptions[i], &configs[i]);
         if (IS_ERR(supplies[i])) {
-            printk(KERN_ERR "Unable to register power supply %d in fake_battery\n", i);
+            printk(KERN_ERR "Unable to register power supply %d in pisugar_3_battery\n", i);
             goto error;
         }
     }
 
-    printk(KERN_INFO "loaded fake_battery module\n");
+    printk(KERN_INFO "loaded pisugar_3_battery module\n");
     return 0;
 
 error:
+    if (pisugar_3_monitor_task) {
+        kthread_stop(pisugar_3_monitor_task);
+        pisugar_3_monitor_task = NULL;
+    }
     while (--i >= 0) {
         power_supply_unregister(supplies[i]);
     }
-    misc_deregister(&control_device);
     return -1;
 }
 
-static void __exit fake_battery_exit(void)
+static void __exit pisugar_3_battery_exit(void)
 {
     int i;
 
-    misc_deregister(&control_device);
+    if (pisugar_3_monitor_task) {
+        kthread_stop(pisugar_3_monitor_task);
+        pisugar_3_monitor_task = NULL;
+    }
 
     for (i = ARRAY_SIZE(descriptions) - 1; i >= 0; i--) {
         power_supply_unregister(supplies[i]);
     }
 
-    printk(KERN_INFO "unloaded fake_battery module\n");
+    printk(KERN_INFO "unloaded pisugar_3_battery module\n");
 }
 
-module_init(fake_battery_init);
-module_exit(fake_battery_exit);
+module_init(pisugar_3_battery_init);
+module_exit(pisugar_3_battery_exit);
 
+MODULE_AUTHOR("The PiSugar Team <pisugar.zero@gmail.com>");
+MODULE_DESCRIPTION("PiSugar 3 battery driver");
 MODULE_LICENSE("GPL");
