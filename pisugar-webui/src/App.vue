a4a91aa5af3291d8ae2770b99ee38566af891607
JdaieLin <490844515@qq.com>
2024-12-18 07:56:20 +0800

    feat: webui refactor

---
+++ pisugar-webui/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <MainWindow/>
+  </div>
+</template>
+
+<script>
+import MainWindow from './components/MainWindow'
+
+export default {
+  name: 'App',
+  components: {
+    MainWindow
+  }
+}
+</script>
+
+<style lang="less">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
