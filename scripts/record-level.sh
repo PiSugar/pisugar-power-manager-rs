2a15749b53bfadb09cab82ddab62b43aa24d2e96
铲屎将军 <37292630+wlmh110@users.noreply.github.com>
2025-07-13 13:34:42 +0800

    Update record-level.sh

--- scripts/record-level.sh
+++ scripts/record-level.sh
@@ -2,7 +2,8 @@
 
 while true; do
     level=$(echo get battery | nc -q 1 127.0.0.1 8423)
+    voltage=$(echo get battery_v | nc -q 1 127.0.0.1 8423)
     now=$(date '+%Y-%m-%d %H:%M:%S')
-    echo $now $level >> records.log
+    echo $now $level $voltage >> records.log
     sleep 5
 done
